6188f0ccd40c3de334dce1454e792d44e212c30d
kjy <kjy@kjyui-MacBookPro2021.local>
2022-07-24 19:31:50 +0900

    1번. 문자찾기

---
+++ javaps/Findchars.java
@@ -0,0 +1,60 @@
+package javaps;
+
+import java.util.Scanner;
+
+
+public class Findchars {
+
+    public int solution(String str, char c){
+
+        int cnt = 0;
+        str = str.toUpperCase();
+        c = Character.toUpperCase(c);
+
+        for(char cc : str.toCharArray()){
+            if(cc == c) cnt++;
+        }
+
+        return cnt;
+    }
+
+
+    public static void main(String[] args){
+
+        Findchars f = new Findchars();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c = sc.next().charAt(0);
+        System.out.print(f.solution(str, c));
+    }
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//public class Findchar {
+//
+//    public int solution(String str, char c){
+//
+//        int val = 0;
+//        str = str.toUpperCase();
+//        c = Character.toUpperCase(c);
+//
+//        for(int i = 0; i <str.length(); i++){
+//            if(str.charAt(i) == c) val++;
+//        }
+//        return val;
+//    }
+//
+//
+//    public static void main(String[] args){
+//        Findchar T = new Findchar();
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        String str = sc.next();
+//        char c = sc.next().charAt(0);
+//        System.out.print(T.solution(str, c));
+//
+//    }
+//
+//}
---
+++ javaps/Reverse04.java
@@ -0,0 +1,2 @@
+package javaps;public class Reverse04 {
+}
