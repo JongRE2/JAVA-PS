c9367503f8d67fe5f144a22932f2225fe56c4c60
kjy <kjy5947@gmail.com>
2022-08-19 23:54:14 +0900

    4번. 단어 뒤집기

---
+++ javaps/Str_4.java
@@ -0,0 +1,23 @@
+package javaps;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Str_4 {
+
+    static ArrayList<String> solution(int n, String[] str){
+        ArrayList<String> answer = new ArrayList<>();
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++){
+            str[i] = sc.next();
+        }
+    }
+}
