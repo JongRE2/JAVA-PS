7913ac917f95d7fdf6f954c1577f43d04eeaea6b
kjy <kjy5947@gmail.com>
2022-08-22 10:32:48 +0900

    5번. 특정 문자 뒤집기

--- javaps/Str_5.java
+++ javaps/Str_5.java
@@ -1,9 +1,35 @@
 package javaps;
 
+import java.util.Scanner;
+
 public class Str_5 {
 
-    static void main(String[] args){
+    public static String solution(String str){
 
+        char tmp;
+        char[] charr = str.toCharArray();
+        int lt = 0, rt = charr.length - 1;
+        while(lt < rt){
+            if(!Character.isAlphabetic(charr[lt])){
+                lt++;
+            }else if(!Character.isAlphabetic(charr[rt])){
+                rt--;
+            }else{
+                tmp = charr[lt];
+                charr[lt] = charr[rt];
+                charr[rt] = tmp;
+                lt++; rt--;
+            }
+
+        }
+        String results = String.valueOf(charr);
+        return results;
+    }
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(solution(str));
     }
 
 }
