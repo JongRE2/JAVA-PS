9bd0d8297b7c792c6e72adaa6ffe4ca89c278fdf
kjy <kjy5947@gmail.com>
2022-09-30 23:11:45 +0900

    cau3

---
+++ javaps/Cau3.java
@@ -0,0 +1,18 @@
+//package javaps;
+//
+//import java.util.Scanner;
+//
+//public class Cau3 {
+//
+//    int solution(){
+//
+//    }
+//
+//    public static void main(String[] args){
+//
+//
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//    }
+//}
