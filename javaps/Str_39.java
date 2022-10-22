81453152e164728ba03a74dac079172dcf58e09e
kjy <kjy5947@gmail.com>
2022-10-22 17:15:18 +0900

    39번. 후위식 연산

--- javaps/Str_39.java
+++ javaps/Str_39.java
@@ -1,8 +1,33 @@
 package javaps;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Str_39 {
 
+    static int solution(String str){
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(char x : str.toCharArray()){
+            if(Character.isDigit(x)) stack.push(x - 48);
+            else{
+                int rt = stack.pop();
+                int lt = stack.pop();
+                if(x == '+') stack.push(lt + rt);
+                else if(x == '-') stack.push(lt - rt);
+                else if(x == '*') stack.push(lt*rt);
+                else if(x == '/') stack.push(lt/rt);
+
+            }
+        }//for
+        answer = stack.get(0);
+        return answer;
+    }
     static public void main(String[] args){
+
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
 
     }
 }
