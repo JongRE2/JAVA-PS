89f1886ed3e305d97642b2ad9ac664853949e3b4
kjy <kjy5947@gmail.com>
2022-11-14 03:50:04 +0900

    48번. 중복 확인 (수정)

--- javaps/Str_48.java
+++ javaps/Str_48.java
@@ -3,6 +3,7 @@ package javaps;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
@@ -11,8 +12,14 @@ public class Str_48 {
 
     static void solution(int n, int[] arrs){
 
-        Set<Integer> set = new HashSet<>(arrs);
+        Integer b[] = Arrays.stream(arrs).boxed().toArray(Integer[]::new);
+        Set<Integer> set = new HashSet<Integer>(Arrays.asList(b));
 
+        if(set.size() == n){
+            System.out.println("U");
+        }else{
+            System.out.println("D");
+        }
 
     }
 
@@ -29,4 +36,8 @@ public class Str_48 {
         }
         solution(n, arrs);
     }
+    /*
+    8
+    20 25 52 30 39 33 43 33
+     */
 }
