db5b15d2b702d39c20bccd9bce7a7106171172f9
kjy <kjy5947@gmail.com>
2022-11-15 01:22:48 +0900

    49번. 장난꾸러기 (수정)

--- javaps/Str_49.java
+++ javaps/Str_49.java
@@ -10,20 +10,31 @@ public class Str_49 {
 
     static void solution(int n, int[] arrs){
 
+        int[] copy = new int[n];
+        for (int i = 0; i < n; i++) copy[i] = arrs[i];
 
-
-
+        //선택정렬로 sort
+        int stored;
+        for(int i = 0; i < n - 1; i++){
+            int idx = i;
+            for(int j = i + 1; j < n;j++){
+                if(arrs[idx] > arrs[j]) idx = j;
+            }
+            stored = arrs[idx];
+            arrs[idx] = arrs[i];
+            arrs[i] = stored;
+        }
         boolean check = false;
-        int first = 0, second = 0;
-        for(int i = 0; i < n-1; i++){
-            if(arrs[i] > arrs[i+1]){
-                if(!check){
-                    first = i+1;
+        int first = -1, second = -1;
+        for(int i = 0; i < n; i++){
+            if(copy[i] != arrs[i]){
+                if(!check) {
+                    first = i + 1;
                     check = true;
                 }
-                else second = i+2;
+                else second = i + 1;
             }
-        }//for
+        }
         System.out.println(first + " " + second);
     }
 
@@ -46,5 +57,8 @@ public class Str_49 {
     /*
     9
     120 125 152 130 135 135 143 127 160
+
+    30
+122 123 125 125 128 130 133 137 138 138 140 141 142 143 145 147 149 149 154 154 155 157 161 167 167 167 161 170 173 173
      */
 }
